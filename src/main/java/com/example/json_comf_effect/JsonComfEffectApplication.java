be5fe672654411e236c2f6af8c6b47b8be49e54e
an99 <kisamasyanya@gmail.com>
2023-07-17 09:32:48 +0300

    added tests

--- src/main/java/com/example/json_comf_effect/JsonComfEffectApplication.java
+++ src/main/java/com/example/json_comf_effect/JsonComfEffectApplication.java
@@ -1,13 +1,13 @@
-package com.example.json_comf_effect;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JsonComfEffectApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(JsonComfEffectApplication.class, args);
-    }
-
-}
+//package com.example.json_comf_effect;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JsonComfEffectApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JsonComfEffectApplication.class, args);
+//    }
+//
+//}

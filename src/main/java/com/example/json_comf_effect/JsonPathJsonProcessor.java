727f5a7ed4fc8afb9fef2a74092f8de135b0fa32
AnUser99 <kisamasyanya@gmail.com>
2023-07-18 07:32:21 +0300

    JsonPath implementation

---
+++ src/main/java/com/example/json_comf_effect/JsonPathJsonProcessor.java
@@ -0,0 +1,28 @@
+package com.example.json_comf_effect;
+
+import com.example.json_comf_effect.model.User;
+import com.jayway.jsonpath.JsonPath;
+
+import java.util.Map;
+
+public class JsonPathJsonProcessor implements JsonProcessor {
+    @Override
+    public String toJson(User user) {
+        throw new UnsupportedOperationException("Conversion from POJO to JSON is not suppoorted by JsonPath");
+    }
+
+    @Override
+    public User fromJson(String json) {
+        return JsonPath.parse(json).read("$", User.class);
+    }
+
+    @Override
+    public Map<String, Object> fromJsonAsMap(String json) {
+        return JsonPath.parse(json).read("$.title", Map.class);
+    }
+
+    @Override
+    public String getUserName(String json) {
+        return JsonPath.parse(json).read("$.title", String.class);
+    }
+}
